3e2f91beea877d5b0c510e2475aa5642349f67a7
Artur Brahms <artur.brahms@outlook.com>
2019-10-14 15:17:30 +0200

    v0.5.2 - Public Beta

---
+++ MidiFighter Twister - Custom-Mapping-Edition.control.js
@@ -0,0 +1,140 @@
+// =====================================================================================================================
+// = Midi Fighter Twister (Dj-TechTools) - Controller-Script for Bitwig
+// ---------------------------------------------------------------------------------------------------------------------
+// = Custom-Mapping-Edition - Created by Artur Brahms | Oct-2019
+// = User controls freely mappable via right mouse-click "Learn Controller Assignment..."
+// =====================================================================================================================
+
+// ---------------------------------------------------------------------------------------------------------------------
+// - BASIC SCRIPT-CONFIG
+// ---------------------------------------------------------------------------------------------------------------------
+var SCRIPT_NAME = "Midi Fighter Twister - Custom-Mapping-Edition";
+var SCRIPT_VERSION = "0.5.2";
+var SCRIPT_MANUFACTURER = "DJ TechTools";
+var SCRIPT_AUTHOR = "Artur Brahms";
+var SCRIPT_UID = "dee03aad-c932-48f1-9aee-22b682856f26";
+
+// MidiFighter Twister - Support up to 4 Devices/Pages a 16 Parameters
+var CC_LO = 1;
+var CC_HI = 128;
+
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+// - BASIC CONTROLLER ASSIGNMENT AND MIDI-CONFIG
+// ---------------------------------------------------------------------------------------------------------------------
+
+// API
+loadAPI (2);
+
+// Deprecate-Fail - Remove this if you want to be able to use deprecated methods without causing script to stop.
+host.setShouldFailOnDeprecatedUse (true);
+
+// Define Controller
+host.defineController (SCRIPT_MANUFACTURER, SCRIPT_NAME, SCRIPT_VERSION, SCRIPT_UID, SCRIPT_AUTHOR);
+
+// Define MIDI-Ports
+host.defineMidiPorts (1, 1);
+
+// Add Device - Platform-Independent
+if (host.platformIsWindows ())
+	{
+	host.addDeviceNameBasedDiscoveryPair (["Midi Fighter Twister"], ["Midi Fighter Twister"]);
+	host.addDeviceNameBasedDiscoveryPair (["Midi Fighter Twister #2"], ["Midi Fighter Twister #2"]);
+	host.addDeviceNameBasedDiscoveryPair (["Midi Fighter Twister #3"], ["Midi Fighter Twister #3"]);
+	host.addDeviceNameBasedDiscoveryPair (["Midi Fighter Twister #4"], ["Midi Fighter Twister #4"]);
+	}
+else if (host.platformIsMac ())
+	{
+	host.addDeviceNameBasedDiscoveryPair (["Midi Fighter Twister"], ["Midi Fighter Twister"]);
+	host.addDeviceNameBasedDiscoveryPair (["Midi Fighter Twister #2"], ["Midi Fighter Twister #2"]);
+	host.addDeviceNameBasedDiscoveryPair (["Midi Fighter Twister #3"], ["Midi Fighter Twister #3"]);
+	host.addDeviceNameBasedDiscoveryPair (["Midi Fighter Twister #4"], ["Midi Fighter Twister #4"]);
+	}
+else if (host.platformIsLinux ())
+	{
+	host.addDeviceNameBasedDiscoveryPair (["Midi Fighter Twister"], ["Midi Fighter Twister"]);
+	host.addDeviceNameBasedDiscoveryPair (["Midi Fighter Twister #2"], ["Midi Fighter Twister #2"]);
+	host.addDeviceNameBasedDiscoveryPair (["Midi Fighter Twister #3"], ["Midi Fighter Twister #3"]);
+	host.addDeviceNameBasedDiscoveryPair (["Midi Fighter Twister #4"], ["Midi Fighter Twister #4"]);
+	}
+
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+// - INIT
+// ---------------------------------------------------------------------------------------------------------------------
+function init ()
+	{
+	// Initialize Transport/MIDI
+	transport = host.createTransport ();
+	midiInputPort0 = host.getMidiInPort (0)
+	midiInputPort0.setMidiCallback (onMidi);
+
+	println (SCRIPT_NAME + " initialized" + " (Version: " + SCRIPT_VERSION + " / Host-API: " + host.getHostApiVersion () + " / Platform: " + host.getPlatformType () + ")");
+
+	// Show Init-Popup
+	host.showPopupNotification (SCRIPT_NAME + " initialized!");
+
+	// Create UserControls
+	var controlHolder = initArray (0, 128);
+	userControls = host.createUserControls (CC_HI - CC_LO + 1);
+	for (var ccNumber = (CC_LO-1); ccNumber <= (CC_HI-1); ccNumber++)
+		{
+		// Get Control
+		var control = userControls.getControl (ccNumber);
+		control.setLabel ("CC" + (ccNumber+1));
+		control.markInterested ();
+
+		// Add Observer
+		control.value ().addValueObserver (buildControlObserverFunction (ccNumber, controlHolder));
+		}
+	}
+
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+// - ON-MIDI
+// ---------------------------------------------------------------------------------------------------------------------
+function onMidi (status, data1, data2)
+	{
+	// Console - Print MIDI
+	// println ("MIDI-In: "); printMidi (status, data1, data2);
+
+	// CC-Messages
+	if (isChannelController (status))
+		{
+		// Console - MIDI-IN-CC
+		println ("MIDI-IN-CC :" + data1 + " > " + data2);
+
+		// Check supported CC-Range
+		if (data1 >= CC_LO && data1 <= CC_HI)
+			{
+			// Set Control
+			userControls.getControl ((data1-1)).value().set (data2, 128);
+			}
+		}
+	}
+
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+// - CONTROL-OBSERVER (FUNCTION)
+// ---------------------------------------------------------------------------------------------------------------------
+function buildControlObserverFunction (ccNumber, controlHolder)
+	{
+	return function (ccValue)
+		{
+		// Store
+		controlHolder[ccNumber] = ccValue;
+
+		// Build MidiFighter-Value (100>127)
+		ccValueMF = (ccValue * 127)
+
+		// Send MIDI-CC to Device
+		sendMidi (176, (ccNumber+1), ccValueMF)
+
+		// Console
+		println ("MIDI-CC-OUT: " + (ccNumber+1) + " > " + Math.round (ccValueMF, 2));
+		}
+	}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Midi Fighter Twister - Custom-Mapping-Edition 
+Control Surface Script for Bitwig Studio - Copyright (c) 2019 by Artur Brahms
+
+Simplified Support for the DJ-TechTools MIDI Fighter Twister
+
+- 128 CC-Controls supported
+- Multiply Twister-Devices supported
+- User controls freely mappable via right mouse-click "Learn Controller Assignment..."
+- Visual Feedback on the Twister
